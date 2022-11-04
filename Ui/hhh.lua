d0a6c27afcf2f827234b8d0b33f49460198f303b
Bestxemchua <103951499+Bestxemchua@users.noreply.github.com>
2022-11-04 15:23:31 +0700

    Update hhh.lua

--- Ui/hhh.lua
+++ Ui/hhh.lua
@@ -922,6 +922,46 @@ end)
 end
 end
 end)
+AutoFarm:Toggle("Bring Mobs",false,function(bm)
+        getgenv().bringmob=bm
+UILib:Notification("Notification", "Bring mob", "don't use lock mob")
+ end)
+spawn(function()
+    while wait() do
+        if getgenv().bringmob  then
+            pcall(function()
+       for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
+for x,y in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
+if getgenv().AutoFarm  and v.Name == Mon and (Mon == "Factory Staff [Lv. 800]" or Mon == "Monkey [Lv. 14]" or Mon == "Dragon Crew Warrior [Lv. 1575]" or Mon == "Dragon Crew Archer [Lv. 1600]") and v:FindFirstChild("Humanoid") and v:FindFirstChild("HumanoidRootPart") and v.Humanoid.Health > 0 and (v.HumanoidRootPart.Position - game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position).Magnitude <= 220 then
+v.HumanoidRootPart.Size = Vector3.new(70,70,70)
+                            v.HumanoidRootPart.CFrame = PosMon
+                            v.Humanoid:ChangeState(14)
+                            v.HumanoidRootPart.CanCollide = false
+                            v.Head.CanCollide = false
+                            if v.Humanoid:FindFirstChild("Animator") then
+                                v.Humanoid.Animator:Destroy()
+                            end
+                            sethiddenproperty(game:GetService("Players").LocalPlayer,"SimulationRadius",math.huge)
+                        elseif getgenv().AutoFarm  and v.Name == Mon and v:FindFirstChild("Humanoid") and v:FindFirstChild("HumanoidRootPart") and v.Humanoid.Health > 0 and (v.HumanoidRootPart.Position - game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position).Magnitude <= 275 then
+                            v.HumanoidRootPart.Size = Vector3.new(70,70,70)
+                            v.HumanoidRootPart.CFrame = PosMon
+                            v.Humanoid:ChangeState(14)
+                            v.HumanoidRootPart.CanCollide = false
+                            v.Head.CanCollide = false
+                            if v.Humanoid:FindFirstChild("Animator") then
+                                v.Humanoid.Animator:Destroy()
+                            end
+                            sethiddenproperty(game:GetService("Players").LocalPlayer,"SimulationRadius",math.huge)
+                        
+    
+    
+end
+end
+end
+end)
+end
+end
+end)
                       AutoFarm:Toggle("Fast Attack",false,function(chim)
   getgenv().fast = chim
 UILib:Notification("Notification", "Fast Attack", "Okay")
